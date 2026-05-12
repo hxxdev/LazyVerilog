4560e6d1e2ba2f35e64b5f9ad828a6a242e3dce8
hxxdev <hxx.dev@gmail.com>
2026-05-13 00:53:22 +0900

    removed garbage

--- src/features/hover definition references rename completion signature_help inlay_hints workspace_symbols formatter lint autoinst autoarg autowire autoff autofunc connect code_action.cpp
+++
@@ -1,2 +0,0 @@
-#include "hover definition references rename completion signature_help inlay_hints workspace_symbols formatter lint autoinst autoarg autowire autoff autofunc connect code_action.hpp"
-// hover definition references rename completion signature_help inlay_hints workspace_symbols formatter lint autoinst autoarg autowire autoff autofunc connect code_action stub
--- src/features/hover definition references rename completion signature_help inlay_hints workspace_symbols formatter lint autoinst autoarg autowire autoff autofunc connect code_action.hpp
+++
@@ -1,2 +0,0 @@
-#pragma once
-// hover definition references rename completion signature_help inlay_hints workspace_symbols formatter lint autoinst autoarg autowire autoff autofunc connect code_action stub
